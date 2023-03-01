67b2dbbcedad7f2441136025fca5aef883b1bda3
kazu2100 <shimokazu21@gmail.com>
2023-03-01 20:29:47 +0900

    ad

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,7 +54,8 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        // 'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+        'auth.basic' => \App\Http\Middleware\BasicAuthenticate::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
--- routes/web.php
+++ routes/web.php
@@ -13,27 +13,31 @@ use App\Http\Controllers\ItemController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::middleware('auth.basic')->group(function(){
 
-Route::get('/', [ItemController::class, 'index']);
 
-// // 投稿の作成ページ
-// Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
+  Route::get('/', [ItemController::class, 'index']);
 
-// // 投稿の作成機能
-// Route::post('/items', [ItemController::class, 'store'])->name('items.store');
+  // // 投稿の作成ページ
+  // Route::get('/items/create', [ItemController::class, 'create'])->name('items.create');
 
-// // indexページ
-// Route::get('/items', [ItemController::class, 'index'])->name('items.index');
+  // // 投稿の作成機能
+  // Route::post('/items', [ItemController::class, 'store'])->name('items.store');
 
-// // 投稿の詳細ページ
-// Route::get('/items/{item}', [ItemController::class, 'show'])->name('items.show');
+  // // indexページ
+  // Route::get('/items', [ItemController::class, 'index'])->name('items.index');
 
-// // 投稿の更新ページ
-// Route::get('/items/{item}/edit', [ItemController::class, 'edit'])->name('items.edit');
+  // // 投稿の詳細ページ
+  // Route::get('/items/{item}', [ItemController::class, 'show'])->name('items.show');
 
-// // 投稿の更新機能
-// Route::patch('/items/{item}', [ItemController::class, 'update'])->name('items.update');
+  // // 投稿の更新ページ
+  // Route::get('/items/{item}/edit', [ItemController::class, 'edit'])->name('items.edit');
 
-// // 投稿の削除機能
-// Route::delete('/items/{item}', [ItemController::class, 'destroy'])->name('items.destroy');
-Route::resource('items', ItemController::class);
+  // // 投稿の更新機能
+  // Route::patch('/items/{item}', [ItemController::class, 'update'])->name('items.update');
+
+  // // 投稿の削除機能
+  // Route::delete('/items/{item}', [ItemController::class, 'destroy'])->name('items.destroy');
+  Route::resource('items', ItemController::class);
+
+});
